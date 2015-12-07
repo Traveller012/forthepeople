e1000c9f8bc35fd6f4a468bd789e755007a3886a
suhanisinghal <suhani@Suhanis-MacBook-Air.local>
2015-12-06 23:15:36 -0500

    Added Authenticate activity, progress bar and async requests

---
+++ app/src/main/java/com/example/harish/forthepeople/Authenticate.java
@@ -0,0 +1,228 @@
+package com.example.harish.forthepeople;
+
+/**
+ * Created by suhani on 12/2/15.
+ */
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.widget.ListView;
+import org.json.JSONArray;
+
+
+
+public class Authenticate extends Activity {
+
+    private static final String TAG = "Http Connection";
+
+    private ListView listView = null;
+
+    EditText username;
+    EditText pass;
+    TextView tv;
+
+    private ArrayAdapter arrayAdapter = null;
+
+    private String[] blogTitles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+        tv=(TextView) findViewById(R.id.username);
+        username=(EditText)findViewById(R.id.username);
+        pass=(EditText)findViewById(R.id.password);
+        Button login=(Button)findViewById(R.id.signInButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                showProgress(true);
+                final String url = "http://javatechig.com/api/get_category_posts/?dev=1&slug=android";
+
+                new AsyncHttpTask().execute(url);
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+
+    }
+
+    private View mProgressView;
+    private View mLoginFormView;
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+
+
+    public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            InputStream inputStream = null;
+
+            HttpURLConnection urlConnection = null;
+
+            Integer result = 0;
+            try {
+                /* forming th java.net.URL object */
+                URL url = new URL(params[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                 /* optional request header */
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                /* optional request header */
+                urlConnection.setRequestProperty("Accept", "application/json");
+
+                /* for Get request */
+                urlConnection.setRequestMethod("GET");
+
+                int statusCode = urlConnection.getResponseCode();
+
+                /* 200 represents HTTP OK */
+                if (statusCode ==  200) {
+
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+                    String response = convertInputStreamToString(inputStream);
+
+                    parseResult(response);
+
+                    result = 1; // Successful
+
+                }else{
+                    result = 0; //"Failed to fetch data!";
+                }
+
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            return result; //"Failed to fetch data!";
+        }
+
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            /* Download complete. Lets update UI */
+
+            if(result == 1){
+
+                showProgress(false);
+                Log.d("debug", blogTitles.toString());
+                //arrayAdapter = new ArrayAdapter(Authenticate.this, android.R.layout.simple_list_item_1, blogTitles);
+                Intent myIntent = new Intent(Authenticate.this, ListDebates.class);
+                myIntent.putExtra("username", username.getText()); //Optional parameters
+                Authenticate.this.startActivity(myIntent);
+                //listView.setAdapter(arrayAdapter);
+            }else{
+                Log.e(TAG, "Failed to fetch data!");
+            }
+        }
+    }
+
+
+    private String convertInputStreamToString(InputStream inputStream) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+
+        String line = "";
+        String result = "";
+
+        while((line = bufferedReader.readLine()) != null){
+            result += line;
+        }
+
+            /* Close Stream */
+        if(null!=inputStream){
+            inputStream.close();
+        }
+
+        return result;
+    }
+
+
+    private void parseResult(String result) {
+
+        try{
+            JSONObject response = new JSONObject(result);
+
+            JSONArray posts = response.optJSONArray("posts");
+
+            blogTitles = new String[posts.length()];
+
+            for(int i=0; i< posts.length();i++ ){
+                JSONObject post = posts.optJSONObject(i);
+                String title = post.optString("title");
+
+                blogTitles[i] = title;
+            }
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+}
